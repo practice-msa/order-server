48c7e30a55f61e325eb65e10a6f0409f954c41cd
kamothi <rhkddlf7911@naver.com>
2024-02-23 17:42:53 +0900

    feat: 배송 정보 조회 및 배송 정보 변경 기능 추가

--- src/main/java/msa/orderserver/controller/DeliveryController.java
+++ src/main/java/msa/orderserver/controller/DeliveryController.java
@@ -1,2 +1,31 @@
-package msa.orderserver.controller;public class DeliveryController {
+package msa.orderserver.controller;
+
+import lombok.RequiredArgsConstructor;
+import msa.orderserver.service.DeliveryService;
+import msa.orderserver.vo.delivery.RequestUpdateDelivery;
+import msa.orderserver.vo.delivery.ResponseDelivery;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.ws.rs.PUT;
+
+@RestController
+@RequestMapping("/delivery")
+@RequiredArgsConstructor
+public class DeliveryController {
+    private final DeliveryService deliveryService;
+
+    @GetMapping("/{deliveryId}")
+    ResponseEntity<ResponseDelivery> getDelivery(@PathVariable("deliveryId") Long id){
+        ResponseDelivery responseDelivery = deliveryService.getDeliveryStatus(id);
+        return ResponseEntity.status(HttpStatus.FOUND).body(responseDelivery);
+    }
+
+    @PutMapping("/{deliveryId}")
+    ResponseEntity<String> updateDelivery(@RequestBody RequestUpdateDelivery requestUpdateDelivery,
+                                          @PathVariable("deliveryId") Long id){
+        deliveryService.updateDeliveryStatus(requestUpdateDelivery,id);
+        return ResponseEntity.status(HttpStatus.OK).body("성공적 처리");
+    }
 }
