7edf10c653464d1ef10cf1b2bb0444dc0ff4d6c9
kamothi <rhkddlf7911@naver.com>
2024-03-13 00:04:12 +0900

    Feat: Response Format 추가

---
+++ src/main/java/msa/orderserver/dto/response/ApiResponse.java
@@ -0,0 +1,29 @@
+package msa.orderserver.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+public class ApiResponse<T>{
+    private Boolean isSuccess;
+    private T response;
+    private ErrorResponse errorResponse;
+
+    public ApiResponse(Boolean isSuccess, T response, ErrorResponse errorResponse) {
+        this.isSuccess = isSuccess;
+        this.response = response;
+        this.errorResponse = errorResponse;
+    }
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    public Boolean getIsSuccess() {
+        return isSuccess;
+    }
+
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    public T getResponse() {
+        return response;
+    }
+    @JsonInclude(JsonInclude.Include.ALWAYS)
+    public ErrorResponse getErrorResponse() {
+        return errorResponse;
+    }
+}
---
+++ src/main/java/msa/orderserver/dto/response/ErrorResponse.java
@@ -0,0 +1,12 @@
+package msa.orderserver.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class ErrorResponse {
+    private String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+}
